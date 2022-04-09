3e859b0de227841a946262eb432e609527af0a87
zyan <zbaptis1@jh.edu>
2022-04-09 17:30:09 -0400

    added code for non ELF files

--- magic.cpp
+++ magic.cpp
@@ -11,8 +11,30 @@
 #include <sys/unistd.h>
 #include <fcntl.h>
 
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <vector>
+
 #include "elf_names.h"
 
+using std::ifstream;
+using std::vector;
+using std::cout;
+using std::endl;
+
 int main(int argc, char **argv) {
-  // TODO: implement
+    // TODO: implemen
+    ifstream FILE(argv[1], std::ios::in | std::ios::binary);
+    char isELF[4];
+    FILE.read(bytes, 4);
+    if (!(isELF[1] == 'E' && isELF[2] == 'L' && isELF[3] == 'F')) {
+        cout << "Not an ELF file" << endl; 
+        FILE.close();
+        exit();
+    }
+    // For ELF files
+
 }
+
+
