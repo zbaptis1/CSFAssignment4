7e1cd2805cd59b271604a01832bdcd53eb4bdb24
zyan <zbaptis1@jh.edu>
2022-04-09 18:32:57 -0400

    added a header file with a ELF struct based on wikipedia page and started implementation of array in magic cpp file

--- magic.cpp
+++ magic.cpp
@@ -17,6 +17,7 @@
 #include <vector>
 
 #include "elf_names.h"
+#include "magic.h"
 
 using std::ifstream;
 using std::vector;
@@ -26,14 +27,22 @@ using std::endl;
 int main(int argc, char **argv) {
     // TODO: implemen
     ifstream FILE(argv[1], std::ios::in | std::ios::binary);
-    char isELF[4];
-    FILE.read(isELF, 4);
+    char isELF[17];
+    FILE.read(isELF, 17);
     if (!(isELF[0] == 127 && isELF[1] == 'E' && isELF[2] == 'L' && isELF[3] == 'F')) {
         cout << "Not an ELF file" << endl; 
         FILE.close();
         return 0;
     }
     // For ELF files
+
+    //TODO: need to see if this works, use some of the test files
+    ELFHeader header;
+    for (int i = 0; i < 17; i++) { 
+        header.e_ident[i] = isELF[i]; 
+        cout << "HEADER: " << header.e_ident[i];
+        cout << "\tISELF: " << isELF[i] << endl;
+    }
 
 }
 
